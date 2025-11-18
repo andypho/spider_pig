362f7f905fb0ec6ac278f92969fa20b6c92b0687
andy <andyho280392@gmail.com>
2025-11-18 20:12:30 +1100

    Update .gitignore and mix.exs to include new dependencies and ignore vscode settings

--- mix.exs
+++ mix.exs
@@ -35,37 +35,42 @@ defmodule SpiderPig.MixProject do
   defp elixirc_paths(:test), do: ["lib", "test/support"]
   defp elixirc_paths(_), do: ["lib"]
 
+  defp heroicons_config do
+    [
+      github: "tailwindlabs/heroicons",
+      tag: "v2.2.0",
+      sparse: "optimized",
+      app: false,
+      compile: false,
+      depth: 1
+    ]
+  end
+
   # Specifies your project dependencies.
   #
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.8.1"},
-      {:phoenix_ecto, "~> 4.5"},
+      {:bandit, "~> 1.5"},
+      {:dns_cluster, "~> 0.2.0"},
       {:ecto_sql, "~> 3.13"},
-      {:postgrex, ">= 0.0.0"},
+      {:esbuild, "~> 0.10", runtime: Mix.env() == :dev},
+      {:gettext, "~> 0.26"},
+      {:heroicons, heroicons_config()},
+      {:jason, "~> 1.2"},
+      {:lazy_html, ">= 0.1.0", only: :test},
+      {:phoenix_ecto, "~> 4.5"},
       {:phoenix_html, "~> 4.1"},
+      {:phoenix_live_dashboard, "~> 0.8.3"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:phoenix_live_view, "~> 1.1.0"},
-      {:lazy_html, ">= 0.1.0", only: :test},
-      {:phoenix_live_dashboard, "~> 0.8.3"},
-      {:esbuild, "~> 0.10", runtime: Mix.env() == :dev},
-      {:tailwind, "~> 0.3", runtime: Mix.env() == :dev},
-      {:heroicons,
-       github: "tailwindlabs/heroicons",
-       tag: "v2.2.0",
-       sparse: "optimized",
-       app: false,
-       compile: false,
-       depth: 1},
-      {:swoosh, "~> 1.16"},
+      {:phoenix, "~> 1.8.1"},
+      {:postgrex, ">= 0.0.0"},
       {:req, "~> 0.5"},
+      {:swoosh, "~> 1.16"},
+      {:tailwind, "~> 0.3", runtime: Mix.env() == :dev},
       {:telemetry_metrics, "~> 1.0"},
-      {:telemetry_poller, "~> 1.0"},
-      {:gettext, "~> 0.26"},
-      {:jason, "~> 1.2"},
-      {:dns_cluster, "~> 0.2.0"},
-      {:bandit, "~> 1.5"}
+      {:telemetry_poller, "~> 1.0"}
     ]
   end
 
